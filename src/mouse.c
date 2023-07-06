51aa2b35cddef47d0dc5d0171ee302e6fe6dfcc2
EugenioBugli <eugeniobugli15@gmail.com>
2023-07-06 18:51:32 +0200

    why I receive nothing

--- src/mouse.c
+++ src/mouse.c
@@ -8,11 +8,9 @@
 #define CLICK (1 << 6)
 #define MOVE (1 << 1)
 #define BAUD 1200
-#define MYUBRR (F_CPU/16/BAUD-1)
+#define MYUBRR ((F_CPU/ (16UL * BAUD)) -1)
 
 //using int8_t instead of uint8_t to store negative numbers ( range : -128 : 127 )
-int8_t X_counter = 0;
-int8_t Y_counter = 0;
 
 void UART_init(void) {
   //set baud rate
@@ -27,6 +25,20 @@ void UART_init(void) {
 void UART_transmit(uint8_t data) {
   while( !(UCSR0A & (1 << UDRE0)) ); //polling ---> wait for transmission completed
   UDR0 = data; //transmit
+  //printf("Send: %d\n", data);
+}
+
+void Send_packets(uint8_t lb, uint8_t rb, int8_t dy, int8_t dx) {
+  uint8_t first_pack = 0x80 | (lb << 5) | (rb << 4) ;//| ( (dy >> 6) & 0x03 ) | ( (dx >> 6) );
+  uint8_t second_pack = 0x00;//dx & 0x7F;
+  uint8_t third_pack = 0x00;//dy & 0x7F;
+  //printf("Send First\n");
+  UART_transmit(first_pack);
+  //printf("Send Second\n");
+  UART_transmit(second_pack);
+  //printf("Send Third\n");
+  UART_transmit(third_pack);
+  //printf("packets sent\n");
 }
 
 int main(void){
@@ -66,51 +78,50 @@ int main(void){
   //printf("\n");
   //printf("Initial coordinates: (%d,%d) \n", X_counter, Y_counter);
   //printf("\n");
-  while(1){
-    uint8_t first = (1 << 7);
-    uint8_t second = 0x0;
-    uint8_t third = 0x0;
 
-    if ( (PINB & CLICK) == 0) {
+  
+  int8_t X_counter = 0;
+  int8_t Y_counter = 0;
+
+  while(1){
+
+    /*if ( (PINB & CLICK) == 0) {
       //printf("Right click from pin: %02x \n", (int) PINB);
-      first = first | (1 << 4);
       //_delay_ms(1000);
     }
 
     if ( (PINH & CLICK) == 0) {
       //printf("Left click from pin: %02x \n", (int) PINH);
-      first = first | (1 << 5);
       //_delay_ms(1000);
-    }
+    }*/
 
     if ( (PINF & MOVE) == 0) {
       //printf("Moving forward. Y position: %d\n", Y_counter);
       Y_counter++;
-      //_delay_ms(1000);
+      _delay_ms(1000);
     }
 
     if ( (PINL & MOVE ) == 0) {
       //printf("Moving right. X position: %d\n", X_counter);
       X_counter++;
-      //_delay_ms(1000);
+      _delay_ms(1000);
     }
 
     if ( (PINK & MOVE) == 0) {
       //printf("Moving backward. Y position: %d\n", Y_counter);
       Y_counter--;
-      //_delay_ms(1000);
+      _delay_ms(1000);
     }
 
     if ( (PING & MOVE ) == 0) {
       //printf("Moving left. X position: %d\n", X_counter);
       X_counter--;
-      //_delay_ms(1000);
+      _delay_ms(1000);
     }
-
-    printf("Actual State : \nRight Click: %d\nLeft Click: %d\nX: %d\nY: %d\n\n",(PINB & CLICK) == 0, (PINH & CLICK) == 0, X_counter, Y_counter);
-    UART_transmit(first);
-    UART_transmit(second);
-    UART_transmit(third);
-    _delay_ms(8000);
+    //printf("Actual State : \nRight Click: %d\nLeft Click: %d\nX: %d\nY: %d\n\n",(PINB & CLICK) == 0, (PINH & CLICK) == 0, X_counter, Y_counter);
+    uint8_t lb = (PINH & CLICK) == 0;
+    uint8_t rb = (PINB & CLICK) == 0;
+    _delay_ms(18000);
+    Send_packets(lb, rb, Y_counter, X_counter);
   }
 }
--- src/serial/std_protocol.c
+++ src/serial/std_protocol.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <termios.h>
+#include <stdint.h>
 
 #define BAUD_RATE B1200
 
 int main() {
-    int serial_fd = open("/dev/ttyACM0", O_RDONLY);
+    int serial_fd = open("/dev/ttyACM0", O_RDWR);
     if(serial_fd  <= 0) {
         printf("Error (Open)\n");
         return 0;
@@ -18,25 +19,38 @@ int main() {
     struct termios tty;
     tcgetattr(serial_fd, &tty); // setting stored to tty
     cfsetispeed(&tty, BAUD_RATE); // set baudrate input
-    cfsetospeed(&tty, BAUD_RATE); //set baudrate output
+    cfsetospeed(&tty, BAUD_RATE);
+    tty.c_cflag |= (CLOCAL | CREAD); //terminal operated in local mode and enable char receiving
+    tty.c_cflag &= ~PARENB; // no parity
+    tty.c_cflag &= ~CSTOPB; //one stop bit
+    tty.c_cflag &= ~CSIZE; //clear data size bits
+    tty.c_cflag |= CS7; // 7 data bits
     tcsetattr(serial_fd, TCSANOW, &tty); // apply tty to serial port
 
-    int buffer[3];
+    uint8_t buffer[3];
     int bytes_read;
 
     while(1) {
-
+        printf("read\n");
         bytes_read = read(serial_fd, buffer, sizeof(buffer));
+        printf("postread\n");
         if(bytes_read == -1) {
             printf("Error (read)\n");
             break;
         }
+        printf("read \n");
+        if(bytes_read == 3 ) {
 
-        if(bytes_read == 3 && (buffer[0] & 0x08)) {
-            int dx = (buffer[0] & 0x3F); // 0011 1111
-            int dy = (buffer[1] & 0x3F); // 0011 1111
-            int lb = (buffer[0] & 0x20); // 0010 0000
-            int rb = (buffer[0] & 0x10); // 0001 0000
+            printf("Read Completed \n");
+            printf("buffer 0: %d\n", buffer[0]);
+            printf("buffer 1: %d\n", buffer[1]);
+            printf("buffer 2: %d\n", buffer[2]);
+
+            uint8_t lb = (buffer[0] & 0x20); // 0010 0000
+            uint8_t rb = (buffer[0] & 0x10); // 0001 0000
+            
+            int8_t dx = 0;// (buffer[1] & 0x3F); // 0011 1111
+            int8_t dy = 0;// (buffer[2] & 0x3F); // 0011 1111
 
             if( dx != 0 || dy != 0){
                 printf("Movement: dx = %d , dy = %d \n", dx,dy);
@@ -49,6 +63,10 @@ int main() {
             if(rb != 0) {
                 printf("Right Button pressed: %d \n", rb); //16
             }
+        else if (bytes_read == 0) {
+            printf("No data sleep");
+            usleep(10000);
+        }
         }
     }
 
