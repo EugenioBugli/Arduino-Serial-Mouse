9c92954fea5f60b84a3bc1ee5dbd009bf90bb9a3
EugenioBugli <eugeniobugli15@gmail.com>
2023-07-08 13:37:48 +0200

    dx&dy ok cutecom

--- src/mouse.c
+++ src/mouse.c
@@ -29,7 +29,7 @@ void UART_transmit(uint8_t data) {
 }
 
 void Send_packets(uint8_t lb, uint8_t rb, int8_t dy, int8_t dx) {
-  uint8_t first_pack = 0x80 | (lb << 5) | (rb << 4) | ((dy & 0xC0) << 2) | ((dx & 0xC0));
+  uint8_t first_pack = 0x80 | (lb << 5) | (rb << 4) | ((dy & 0xC0) << 3) | ((dx & 0xC0) << 1);
   uint8_t second_pack = dx & 0x3F;
   uint8_t third_pack = dy & 0x3F;
   //printf("Send First\n");
@@ -83,8 +83,8 @@ int main(void){
   //printf("\n");
 
   
-  uint8_t X_counter = 0;
-  uint8_t Y_counter = 0;
+  int8_t X_counter = 0;
+  int8_t Y_counter = 0;
 
   while(1){
 
--- src/serial/std_protocol.c
+++ src/serial/std_protocol.c
@@ -32,13 +32,12 @@ int main() {
     ssize_t bytes_read;
     int idx = 0;
     while(1) {
-        bytes_read = read(serial_fd, &buffer[idx], sizeof(buffer[idx]));
+        bytes_read = read(serial_fd, buffer, sizeof(buffer));
         if(bytes_read == -1) {
             printf("Error during Read\n");
             break;
         }
-        if(bytes_read > 0) {
-            // check data
+        if(bytes_read == sizeof(buffer)) {
             if((buffer[0] & 0x80) && !(buffer[1] & 0x80) && !(buffer[2] & 0x80) ) {
                 uint8_t first = buffer[0];
                 printf("First packet: %u\n", first);
@@ -49,11 +48,11 @@ int main() {
                 idx = 0;
                 uint8_t lb = first & 0x10;
                 uint8_t rb = first & 0x20;
-                uint8_t dx = second & 0x3F;
-                uint8_t dy = third & 0x3F;
+                int8_t dx = (second & 0x3F);
+                int8_t dy = (third & 0x3F);
                 if(lb) printf("Left click\n");
                 if(rb) printf("Right Click\n");
-                printf("current position: %u,%u\n ", dx, dy);
+                printf("current position: %d,%d\n ", dx, dy);
                 printf("\n------\n");
             }
         }
